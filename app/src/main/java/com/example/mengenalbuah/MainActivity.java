b5700f69e80b97e9b133e721c7fa8604b0c8099e
Muhammad Azhar <azhardevelop@gmail.com>
2019-10-07 08:08:01 +0700

    Update MainActivity.java
    Tambahin kode java buat Horizontal ProgressBar

--- app/src/main/java/com/example/mengenalbuah/MainActivity.java
+++ app/src/main/java/com/example/mengenalbuah/MainActivity.java
@@ -6,18 +6,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+     private int Value = 0;
+     private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        progressBar = findViewById(R.id.horizontalProg);
+        progressBar.setProgress(0)
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, MenuScreen.class));
-                finish();
+                if (Value == progressBar.getMax()){
+                    Toast.makeText(getApplicationContext(), "Progress Completed", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, MenuScreen.class));
+                    finish();
             }
         },3000);
     }
